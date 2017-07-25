04fae0ba066397880b5f6da326c9079d6ec1627c
mdesson <mdesson@gmail.com>
2017-07-24 23:27:35 -0400

    On page 72, starting 'Category View'

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,16 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 
 def index(request):
-    # Construct a dictionary to pass to the template as its  context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by number of likes in descending order.
+    # Retrive the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary.
+    # that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     context_dict = {'boldmessage': "created for a Django Tutorial!"}
