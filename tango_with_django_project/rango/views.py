391541768d861bbceef5c08016abb0562dd957e7
mdesson <mdesson@gmail.com>
2017-08-06 22:17:18 -0400

    Created category.html template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
@@ -22,3 +23,35 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': "created for a Django Tutorial!"}
     return render(request, 'rango/about.html', context=context_dict)
+
+
+def show_category(request, category_name_slug):
+    # Create a context dict which we can pass to the toempalte rendering engine
+    context_dict = {}
+
+    try:
+        # Can we find a category naem slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception
+        # So the .get() method returns one model instance or raises an exception.
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Retrieve all of the associated pages
+        # Note that filter() will return a list of page objects or an empty list.
+        pages = Page.objects.filter(category=category)
+
+        # Adds our results list to the template context under the name pages.
+        context_dict['pages'] = pages
+        # We also add the category object from
+        # the database to the context dict.
+        # We'll use this in teh tempalte to verify that the category exists.
+        context_dict['category'] = category
+
+    except:
+        # We get here if we didn't find the specified category.
+        # Don't do anything.
+        # the template will display the "no category" message for us.
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    # Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context_dict)
