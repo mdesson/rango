b753adb0bb685dc96ccc7b1c5b604cd4ea281c46
mdesson <mdesson@gmail.com>
2017-08-07 22:50:42 -0400

    Completed Chapter 6

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -12,25 +12,33 @@ from rango.models import Category, Page
 def populate():
     python_pages = [
         {"title": "Official Python Tutorial",
-         "url": "https://docs.python.org/3/tutorial/"},
+         "url": "https://docs.python.org/3/tutorial/",
+         "views": 16},
         {"title": "How to Think Like a Computer Scientist",
-         "url": "http://greenteapress.com/thinkpython/"},
+         "url": "http://greenteapress.com/thinkpython/",
+         "views": 81},
         {"title": "Learn Python in 10 Minutes",
-         "url": "http://www.korokithakis.net/tutorials/python/"}]
+         "url": "http://www.korokithakis.net/tutorials/python/",
+         "views": 18}]
 
     django_pages = [
         {"title": "Official Django Tutorial",
-         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 41},
         {"title": "Django Rocks",
-         "url": "http://ww.djangorocks.com/"},
+         "url": "http://ww.djangorocks.com/",
+         "views": 14},
         {"title": "How to Tango with Django",
-         "url": "http://www.tangowithdjango.com/"}]
+         "url": "http://www.tangowithdjango.com/",
+         "views": 21}]
 
     other_pages = [
         {"title": "Bottle",
-         "url": "http://bottlepy.org/docs/dev/"},
+         "url": "http://bottlepy.org/docs/dev/",
+         "views": 12},
         {"title": "Flask",
-         "url": "http://flask.pocoo.org"}]
+         "url": "http://flask.pocoo.org",
+         "views": 1}]
 
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
             "Django": {"pages": django_pages, "views": 64, "likes": 32},
@@ -41,7 +49,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     # print out the categories we added
     for c in Category.objects.all():
@@ -49,8 +57,8 @@ def populate():
             print("- {0} - {1}".format(str(c), str(p)))
 
 
-def add_page(cat, title, url):
-    p = Page.objects.get_or_create(category=cat, title=title)[0]
+def add_page(cat, title, url, views):
+    p = Page.objects.get_or_create(category=cat, title=title, views=views)[0]
     p.url = url
 
     p.save()
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,13 +6,14 @@ from rango.models import Page
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
-    # Order the categories by number of likes in descending order.
+    # Order the categories & pages by number of likes in descending order.
     # Retrive the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary.
     # that will be passed to the template engine.
 
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
@@ -30,7 +31,7 @@ def show_category(request, category_name_slug):
     context_dict = {}
 
     try:
-        # Can we find a category naem slug with the given name?
+        # Can we find a category name slug with the given name?
         # If we can't, the .get() method raises a DoesNotExist exception
         # So the .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
